487b982b8e04dfd61afe65b1b9d4bd879fa84c24
LEDUNOIS Simon <simon.ledunois@gmail.com>
2022-02-03 13:19:14 +0000

    chore: add pre commit hook

---
+++ scripts/init.sh
@@ -0,0 +1,12 @@
+# pre-commit installation
+curl https://pre-commit.com/install-local.py | python -
+source ~/.profile
+
+pip install pre-commit
+
+pre-commit --version
+pre-commit install
+
+npm install --save-dev @commitlint/{config-conventional,cli} husky
+npx husky install
+npx husky add .husky/commit-msg "npx --no -- commitlint --edit \"$1\""
